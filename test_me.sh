afdd8a6371fc841d9a391ff671424e74a9080c61
ankitha995 <61868177+ankitha995@users.noreply.github.com>
2020-03-18 22:31:20 +0530

    Update and rename q2 to test_me.sh

--- test_me.sh
+++ test_me.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# save as test_me.sh
+
 if [ $# -ge 1 ] then             # checking whether the attributes given is greater than or equal to 1 in if condition
 
         echo "this is a test"     
